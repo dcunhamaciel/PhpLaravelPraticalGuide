34521e4822800f0f1e94beb17888fcbd4772d045
dcunhamaciel <dcunhamaciel@gmail.com>
2023-09-02 16:41:25 -0300

    Login'

--- laravel-admin/app/Http/Controllers/AuthController.php
+++ laravel-admin/app/Http/Controllers/AuthController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+
 
 class AuthController extends Controller
 {
@@ -19,5 +22,23 @@ class AuthController extends Controller
         ]);
 
         return response($user, Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            return response([
+                'error' => 'Invalid credentials!'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        /** @var User $user */
+        $user = Auth::user();
+
+        $token = $user->createToken('token')->plainTextToken;
+        
+        return response([
+            'jwt' => $token
+        ], Response::HTTP_OK);
     }
 }

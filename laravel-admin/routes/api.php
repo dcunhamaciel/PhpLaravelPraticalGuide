5270c35dc1e23b5f0ab6b3be1637f0e870a286b4
dcunhamaciel <dcunhamaciel@gmail.com>
2023-09-07 11:08:48 -0300

    API Resources

--- laravel-admin/routes/api.php
+++ laravel-admin/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +21,12 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::get('user', [AuthController::class, 'user']);
+
+    //Route::get('users', [UserController::class, 'index']);
+    //Route::post('users', [UserController::class, 'store']);
+    //Route::get('users/{id}', [UserController::class, 'show']);
+    //Route::put('users/{id}', [UserController::class, 'update']);
+    //Route::delete('users/{id}', [UserController::class, 'destroy']);
+
+    Route::apiResource('users', UserController::class);
 });

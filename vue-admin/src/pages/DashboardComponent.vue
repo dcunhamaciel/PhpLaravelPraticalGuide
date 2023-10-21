b7ab3e3a447ef1959a946cfdc573d22142834707
dcunhamaciel <dcunhamaciel@gmail.com>
2023-10-21 11:27:00 -0300

    Chart

--- vue-admin/src/pages/DashboardComponent.vue
+++ vue-admin/src/pages/DashboardComponent.vue
@@ -1,9 +1,48 @@
 <template>
-    Dashboard
+    <h2>Daily Sales</h2>
+    <div id="chart"></div>
 </template>
 
-<script>
+<script lang="ts">
+import { onMounted } from 'vue'
+import axios from 'axios'
+import * as c3 from 'c3'
+
 export default {
-    name: 'DashboardComponent'
+    name: 'DashboardComponent',
+    setup() {
+        onMounted(async () => {
+            const chart = c3.generate({
+                bindto: '#chart',
+                data: {
+                    x: 'x',
+                    columns: [
+                        ['x'],
+                        ['Sales']
+                    ],
+                    types: {
+                        Sales: 'bar'
+                    }
+                },
+                axis: {
+                    x: {
+                        type: 'timeseries',
+                        tick: {
+                            format: '%Y-%m-%d'
+                        }
+                    }
+                }
+            })
+
+            const { data } = await axios.get('chart')
+
+            chart.load({
+                columns: [
+                    ['x', ...data.map((r: any) => r.date)],
+                    ['Sales', ...data.map((r: any) => r.sum)]
+                ]
+            })
+        })
+    }
 }
 </script>
